8397afe7f8cc0f9f8ee2529c66b41a9b389a7dde
man.luo <man.luo@handeson.com>
2020-01-07 18:24:48 +0800

    first commit

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"go-kafka/src"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
-func main() {
-	var conf src.Config
+var (
+	conf  src.Config
+	mode  string
+	topic string
+)
+
+func init() {
+	flag.StringVar(&mode, "mode", "producer", "producer or consumer")
+	flag.StringVar(&topic, "topic", "go_test", "topic name")
+	flag.Parse()
 
 	yamlFile, _ := ioutil.ReadFile("config.yaml")
 	err := yaml.Unmarshal(yamlFile, &conf)
@@ -15,6 +28,50 @@ func main() {
 		panic(err)
 	}
 
+}
+
+func main() {
+	if mode == "producer" {
+		producer()
+	} else {
+		consumer()
+	}
+}
+
+func producer() {
 	producer, _ := src.NewProducer(conf, "go_test", 0)
 	_ = producer.Push([]byte("{\"name\":\"test\"}"), nil)
+}
+
+func consumer() {
+	var topics = []src.Topic{
+		{
+			Name: "go_test",
+		},
+	}
+	consumer, _ := src.NewConsumer(conf, "test-id", topics)
+
+	loop := true
+	go func() {
+		var c = make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGQUIT, syscall.SIGTERM, syscall.SIGINT)
+		<-c
+
+		fmt.Println("quit")
+		loop = false
+	}()
+
+	for loop {
+		data, err := consumer.Pull(200)
+		if err != nil {
+			panic(err)
+		}
+
+		if data == "" {
+			continue
+		}
+
+		fmt.Println(data)
+		_ = consumer.Commit()
+	}
 }
--- src/consumer.go
+++ src/consumer.go
@@ -1,1 +1,97 @@
 package src
+
+import (
+	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"sync"
+)
+
+type Consumer struct {
+	Handler  *kafka.Consumer
+	Messages []*kafka.Message
+	Mux      sync.Mutex
+}
+
+type Topic struct {
+	Name      string
+	Partition int32
+	Offset    int64
+}
+
+func (consumer *Consumer) Pull(timeout int) (string, error) {
+	ev := consumer.Handler.Poll(timeout)
+	if ev == nil {
+		return "", nil
+	}
+
+	switch e := ev.(type) {
+	case *kafka.Message:
+		consumer.Messages = append(consumer.Messages, e)
+		return string(e.Value), nil
+	case kafka.Error:
+		return "", e
+	default:
+		return "", nil
+	}
+}
+
+func (consumer *Consumer) Commit() error {
+	defer consumer.Mux.Unlock()
+	consumer.Mux.Lock()
+
+	for i := 0; len(consumer.Messages) > 0; {
+		if _, err := consumer.Handler.CommitMessage(consumer.Messages[i]); err != nil {
+			return err
+		}
+		consumer.Messages = append(consumer.Messages[i+1:])
+	}
+
+	return nil
+}
+
+func (consumer *Consumer) Close() error {
+	if err := consumer.Handler.Close(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewConsumer(config Config, groupId string, topics []Topic) (*Consumer, error) {
+	(*config)["group.id"] = groupId
+	c, err := kafka.NewConsumer(config)
+	if err != nil {
+		return nil, err
+	}
+
+	topicPartitions := make([]kafka.TopicPartition, 0, len(topics))
+	topicNames := make([]string, 0, len(topics))
+
+	for _, topic := range topics {
+		if topic.Offset == 0 {
+			topicNames = append(topicNames, topic.Name)
+		} else {
+			topicPartitions = append(topicPartitions, kafka.TopicPartition{
+				Topic:     &topic.Name,
+				Partition: topic.Partition,
+				Offset:    kafka.Offset(topic.Offset),
+			})
+		}
+	}
+
+	if len(topicNames) > 0 {
+		if err = c.SubscribeTopics(topicNames, nil); err != nil {
+			return nil, err
+		}
+	}
+
+	if len(topicPartitions) > 0 {
+		if err = c.Assign(topicPartitions); err != nil {
+			return nil, err
+		}
+	}
+
+	consumer := &Consumer{
+		Handler: c,
+	}
+	return consumer, nil
+}
